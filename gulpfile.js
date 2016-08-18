d1cc7400115e5d5832dd82dee976c46552d56990
kobe <471816299@qq.com>
2016-08-18 17:42:45 +0800

    seventh commit

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,14 @@
     var uglify = require('gulp-uglify');
     var react = require('gulp-react');
     var gulpif = require('gulp-if');
-    var babel = require('gulp-babel');
+    var babel = require('gulp-babel');;
+    var rename = require("gulp-rename");
+    var browserify=require('browserify');
+    var sourcemaps = require("gulp-sourcemaps");
+    var buffer = require('gulp-buffer');
+    var tap  = require('gulp-tap');
     var path = {
-        HTML: 'app/src/index.html'
+        HTML: 'app/src/*.html'
         , ALL: ['app/src/js/*.js', 'app/src/js/**/*.js', 'app/src/index.html']
         , JS: ['app/src/js/*.js', 'app/src/js/**/*.js']
         , MINIFIED_OUT: 'build.min.js'
@@ -22,21 +27,34 @@
     })
 
     //app/src/*.html中的js,合并压缩成一个js,替换app/src/*html中的js并且输出到app/dist
-    gulp.task('replaceHTML', ['babel'],function(){
+    gulp.task('replaceHTML',function(){
        gulp.src(path.HTML)
        .pipe(useref())
+       .pipe(gulpif('*.js',babel({
+           presets: ['react']
+       })))
+       .pipe(gulpif('*.js',babel({
+           presets: ['es2015']
+       })))
+       .pipe(gulpif('*.js',gulp.dest('app/src/')))
        .pipe(gulp.dest(path.DEST));
     });
-    
-    gulp.task('babel', function () {
-        return gulp.src(path.JS)
-        .pipe(babel({
-            presets: ['react']
-        }))
-        .pipe(babel({
-            presets: ['es2015']
-        }))
+
+    gulp.task('babel',function () {
+        return gulp.src('app/src/js/*.js')
         .pipe(gulp.dest('app/src/build'));
+    });
+
+    gulp.task('browserify', function(done) {
+        return gulp.src('app/src/build/*.js', {read: false}) // no need of reading file because browserify does.
+          .pipe(tap(function (file) {
+            file.contents = browserify(file.path, {debug: true}).bundle();
+          }))
+          .pipe(buffer())
+          .pipe(sourcemaps.init({loadMaps: true}))
+          .pipe(uglify())
+          .pipe(sourcemaps.write('./'))
+          .pipe(gulp.dest('app/dist/build'));
     });
 
     //发布到生产环境的task
